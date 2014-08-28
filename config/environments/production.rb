18f5084e7620971bfbc1d4a726075528112fc281
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2014-08-28 16:06:07 -0500

    Change domain in production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       :address              => 'smtp.sendgrid.net',
       :port                 => 587,
-      :domain               => "heroku.com",
+      :domain               => "rahm.com",
       :user_name            => ENV['SENDGRID_USERNAME'],
       :password             => ENV['SENDGRID_PASSWORD'],
       :authentication       => :plain,
